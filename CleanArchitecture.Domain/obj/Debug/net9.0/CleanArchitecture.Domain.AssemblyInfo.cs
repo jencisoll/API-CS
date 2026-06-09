62f42b0f3b6204f196e2694dd622c7dbe1642101
Jerry <jencisollallahui@gmail.com>
2026-06-09 00:24:58 -0500

    Agrega archivo .gitignore para limpiar el entorno

--- CleanArchitecture.Domain/obj/Debug/net9.0/CleanArchitecture.Domain.AssemblyInfo.cs
+++ CleanArchitecture.Domain/obj/Debug/net9.0/CleanArchitecture.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e3a32b7eb26a36328e6928ba17d8ccd6d75546b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f24873b59e6a8dd9436109b0cde1ab5d642b07bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchitecture.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
